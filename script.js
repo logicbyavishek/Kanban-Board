3121436bf551e0d2bc457c80118c714402c9c875
AVISHEK DUTTA <109311355+logicbyavishek@users.noreply.github.com>
2026-01-22 03:42:10 +0530

    Code modularity Increase
    all features done but i want to add some my test in this project although add something but i want to change font theme and also add edit wala feature

--- script.js
+++ script.js
@@ -7,30 +7,80 @@ const columns = [todo, progress, done];
 
 let draggedItem = null; // create a variable to store the reference of the dragged item
 
+// Modal functionality Logic------------->>>>
+const toggleModalButton = document.querySelector('#toggle-modal');
+const modal = document.querySelector('.modal');
+const bg = document.querySelector('.bg');
+
+toggleModalButton.addEventListener('click',()=>{
+    modal.classList.toggle('active');
+})
+
+bg.addEventListener('click',()=>{
+    modal.classList.toggle('active');
+    // Clear the input fields
+    document.querySelector('#task-titel-input').value='';
+    document.querySelector('#task-desc-input').value='';
+})
+// <<<<<<<<<--------------- End of Modal functionality Logic
+
+function addTask(titel,column,desc){
+    const div = document.createElement('div');
+    div.classList.add('task');
+    div.setAttribute('draggable','true');
+    div.innerHTML=`         <h2>${titel}</h2>
+                            <p>${desc}</p>
+                            <button>Delete</button>`;
+    column.appendChild(div);
+    // Add drag event to the newly created task
+    div.addEventListener('drag',(e)=>{
+        draggedItem = div;
+    })
+
+    document.querySelector('#task-titel-input').value='';
+    document.querySelector('#task-desc-input').value='';
+
+    modal.classList.remove('active');
+
+    // Add delete functionality to the delete button
+    const deleteButton = div.querySelector('button');
+    deleteButton.addEventListener('click',()=>{
+        div.remove();
+        updateTaskCount();
+    })
+
+    return div;
+}
+
+function updateTaskCount(){
+    // Update task count in todo column
+        columns.forEach(col=>{
+            const task = col.querySelectorAll('.task');
+            const count = col.querySelector('.right')
+
+            // Update tasksData object 
+            tasksData[col.id]=Array.from(task).map(t=>{
+                return {
+                    title: t.querySelector('h2').innerText,
+                    desc: t.querySelector('p').innerText
+                }
+            });
+
+            localStorage.setItem('tasksData',JSON.stringify(tasksData));
+
+            count.textContent = task.length;
+        })
+}
 
 if(localStorage.getItem('tasksData')){
     const data = JSON.parse(localStorage.getItem('tasksData'));
-    console.log(data);
     for(const col in data){
         const column = document.querySelector(`#${col}`);
         data[col].forEach(taskData=>{
-            const div = document.createElement('div');
-            div.classList.add('task');
-            div.setAttribute('draggable','true');
-            div.innerHTML=`         <h2>${taskData.title}</h2>
-                                    <p>${taskData.desc}</p>
-                                    <button>Delete</button>`;
-            column.appendChild(div);
-            // Add drag event to the newly created task
-            div.addEventListener('drag',(e)=>{
-                draggedItem = div;
-            })
+            addTask(taskData.title,column,taskData.desc);
         })
-
-        const task = column.querySelectorAll('.task');
-        const count = column.querySelector('.right');
-        count.textContent = task.length;
     }
+    updateTaskCount();
 }
 
 
@@ -65,22 +115,7 @@ function DragAndDrop(column){
         column.classList.remove('hover-over'); // remove the hover effect once dropped
 
     // Update task count in todo column
-        columns.forEach(col=>{
-            const task = col.querySelectorAll('.task');
-            const count = col.querySelector('.right')
-
-            // Update tasksData object 
-            tasksData[col.id]=Array.from(task).map(t=>{
-                return {
-                    title: t.querySelector('h2').innerText,
-                    desc: t.querySelector('p').innerText
-                }
-            });
-
-            localStorage.setItem('tasksData',JSON.stringify(tasksData));
-
-            count.textContent = task.length;
-        })
+        updateTaskCount();
 
     })
 }
@@ -89,19 +124,6 @@ DragAndDrop(todo);
 DragAndDrop(progress);
 DragAndDrop(done);
 
-// Modal functionality Logic------------->>>>
-const toggleModalButton = document.querySelector('#toggle-modal');
-const modal = document.querySelector('.modal');
-const bg = document.querySelector('.bg');
-
-toggleModalButton.addEventListener('click',()=>{
-    modal.classList.toggle('active');
-})
-
-bg.addEventListener('click',()=>{
-    modal.classList.toggle('active');
-})
-// <<<<<<<<<--------------- End of Modal functionality Logic
 
 // Add Task Functionality Logic--------->>>>
 const addTaskButton = document.querySelector('#add-new-task');
@@ -110,36 +132,10 @@ addTaskButton.addEventListener('click',()=>{
     const taskTitle=document.querySelector('#task-titel-input').value;
     const taskDesc=document.querySelector('#task-desc-input').value;
 
-    const div = document.createElement('div');
-    div.classList.add('task');
-    div.setAttribute('draggable','true');
-    div.innerHTML=`         <h2>${taskTitle}</h2>
-                            <p>${taskDesc}</p>
-                            <button>Delete</button>`;
-    todo.appendChild(div);
+    addTask(taskTitle,todo,taskDesc);
 
     // Update task count in todo column
-    columns.forEach(col=>{
-            const task = col.querySelectorAll('.task');
-            const count = col.querySelector('.right')
-
-            // Update tasksData object 
-            tasksData[col.id]=Array.from(task).map(t=>{
-                return {
-                    title: t.querySelector('h2').innerText,
-                    desc: t.querySelector('p').innerText
-                }
-            });
-
-            localStorage.setItem('tasksData',JSON.stringify(tasksData));
-
-            count.textContent = task.length;
-        })
-
-    // Add drag event to the newly created task
-    div.addEventListener('drag',(e)=>{
-        draggedItem = div;
-    })
+    updateTaskCount();
 
     // Clear the input fields
     document.querySelector('#task-titel-input').value='';
